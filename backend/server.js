abfa6fd3b34c71808db10a20dd33e9760f5f1dbf
Yogeshbisht65 <152010856+Yogeshbisht65@users.noreply.github.com>
2025-08-08 11:52:58 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,13 @@ const allowedOrigins = [
   'https://www.codewithyash.me',
 ];
 
+a
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+  origin: ['http://localhost:5173', 'https://codewithyash.me'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
